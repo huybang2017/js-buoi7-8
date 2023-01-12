196db483d2d73f2ea8d288a5c7995a3c24a6f74e
huybang2017 <ndhbang2017@gmail.com>
2023-01-12 14:55:29 +0700

    fixing

---
+++ index.js
@@ -0,0 +1,58 @@
+// INPUT
+let input__num = +document.getElementById("input__num").value
+let input__btn = document.getElementById("input__btn")
+let input__alert = document.getElementById("input__alert")
+// SUM
+let btn__sum = document.getElementById("btn__sum")
+let sum__btn = document.getElementById("sum__btn")
+let sum__form = document.getElementById("sum__form")
+let sum__alert = document.getElementById("sum__alert")
+// ĐẾM
+let btn__count = document.getElementById("btn__count")
+let count__btn = document.getElementById("count__btn")
+let count__form = document.getElementById("count__form")
+let count__alert = document.getElementById("count__alert")
+// TÌM SỐ NHỎ NHẤT
+let btn__findMin = document.getElementById("btn__findMin")
+let findMin__btn = document.getElementById("findMin__btn")
+let findMin__form = document.getElementById("findMin__form")
+let findMin__alert = document.getElementById("findMin__alert")
+// TÌM SỐ DƯƠNG NHỎ NHẤT
+let btn__findMinPositive = document.getElementById("btn__findMinPositive")
+let findMinPositive__btn = document.getElementById("findMinPositive__btn")
+let findMinPositive__form = document.getElementById("findMinPositive__form")
+let findMinPositive__alert = document.getElementById("findMinPositive__alert")
+// TÌM SỐ CHẴN CUỐI CÙNG
+let btn__numEven = document.getElementById("btn__numEven")
+let numEven__btn = document.getElementById("numEven__btn")
+let numEven__form = document.getElementById("numEven__form")
+let numEven__alert = document.getElementById("numEven__alert")
+// ĐỔI CHỖ
+let btn__translocate = document.getElementById("btn__translocate")
+let translocate__btn = document.getElementById("translocate__btn")
+let translocate__num1 = +document.getElementById("translocate__num1").value
+let translocate__num2 = +document.getElementById("translocate__num2").value
+let translocate__form = document.getElementById("translocate__form")
+let translocate__alert = document.getElementById("translocate__alert")
+// SẮP XẾP
+let btn__arrange = document.getElementById("btn__arrange")
+let arrange__btn = document.getElementById("arrange__btn")
+let arrange__form = document.getElementById("arrange__form")
+let arrange__alert = document.getElementById("arrange__alert")
+// TÌM SỐ NGUYÊN TỐ ĐẦU TIÊN
+let btn__Prime = document.getElementById("btn__Prime")
+let Prime__btn = document.getElementById("Prime__btn")
+let Prime__form = document.getElementById("Prime__form")
+let Prime__alert = document.getElementById("Prime__alert")
+// ĐẾM SỐ NGUYÊN
+let btn__countIntegers = document.getElementById("btn__countIntegers")
+let addNum = +document.getElementById("addNum").value
+let addNum__btn = document.getElementById("addNum__btn")
+let countIntegers__btn = document.getElementById("countIntegers__btn")
+let countIntegers__form = document.getElementById("countIntegers__form")
+let countIntegers__alert = document.getElementById("countIntegers__alert")
+// SO SÁNH
+let btn__compare = document.getElementById("btn__compare")
+let compare__btn = document.getElementById("compare__btn")
+let compare__form = document.getElementById("compare__form")
+let compare__alert = document.getElementById("compare__alert")

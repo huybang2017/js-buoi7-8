b7aa9c413fec290597a9aecb1257527f95a86827
huybang2017 <ndhbang2017@gmail.com>
2023-01-17 15:16:09 +0700

    fixing

--- index.js
+++ index.js
@@ -1,58 +1,216 @@
+function getElement(selector) {
+    return document.querySelectorAll(selector);
+}
+// function mở form
+function openForm(compareVar) {
+    for (let i = 1; i < formEl.length; i++) {
+        if (i === compareVar) {
+            formEl[i].classList.toggle("d-block");
+        } else {
+            formEl[i].classList.remove("d-block");
+        }
+    }
+}
+let formEl = getElement("form");
+let numArray = [];
+
 // INPUT
-let input__num = +document.getElementById("input__num").value
-let input__btn = document.getElementById("input__btn")
-let input__alert = document.getElementById("input__alert")
+function addAll() {
+    let input__num = +document.getElementById("input__num").value;
+    numArray.push(input__num);
+    let input__alert = document.getElementById("input__alert");
+    input__alert.innerHTML = `${numArray}`;
+}
 // SUM
-let btn__sum = document.getElementById("btn__sum")
-let sum__btn = document.getElementById("sum__btn")
-let sum__form = document.getElementById("sum__form")
-let sum__alert = document.getElementById("sum__alert")
-// ĐẾM
-let btn__count = document.getElementById("btn__count")
-let count__btn = document.getElementById("count__btn")
-let count__form = document.getElementById("count__form")
-let count__alert = document.getElementById("count__alert")
-// TÌM SỐ NHỎ NHẤT
-let btn__findMin = document.getElementById("btn__findMin")
-let findMin__btn = document.getElementById("findMin__btn")
-let findMin__form = document.getElementById("findMin__form")
-let findMin__alert = document.getElementById("findMin__alert")
-// TÌM SỐ DƯƠNG NHỎ NHẤT
-let btn__findMinPositive = document.getElementById("btn__findMinPositive")
-let findMinPositive__btn = document.getElementById("findMinPositive__btn")
-let findMinPositive__form = document.getElementById("findMinPositive__form")
-let findMinPositive__alert = document.getElementById("findMinPositive__alert")
-// TÌM SỐ CHẴN CUỐI CÙNG
-let btn__numEven = document.getElementById("btn__numEven")
-let numEven__btn = document.getElementById("numEven__btn")
-let numEven__form = document.getElementById("numEven__form")
-let numEven__alert = document.getElementById("numEven__alert")
-// ĐỔI CHỖ
-let btn__translocate = document.getElementById("btn__translocate")
-let translocate__btn = document.getElementById("translocate__btn")
-let translocate__num1 = +document.getElementById("translocate__num1").value
-let translocate__num2 = +document.getElementById("translocate__num2").value
-let translocate__form = document.getElementById("translocate__form")
-let translocate__alert = document.getElementById("translocate__alert")
-// SẮP XẾP
-let btn__arrange = document.getElementById("btn__arrange")
-let arrange__btn = document.getElementById("arrange__btn")
-let arrange__form = document.getElementById("arrange__form")
-let arrange__alert = document.getElementById("arrange__alert")
-// TÌM SỐ NGUYÊN TỐ ĐẦU TIÊN
-let btn__Prime = document.getElementById("btn__Prime")
-let Prime__btn = document.getElementById("Prime__btn")
-let Prime__form = document.getElementById("Prime__form")
-let Prime__alert = document.getElementById("Prime__alert")
-// ĐẾM SỐ NGUYÊN
-let btn__countIntegers = document.getElementById("btn__countIntegers")
-let addNum = +document.getElementById("addNum").value
-let addNum__btn = document.getElementById("addNum__btn")
-let countIntegers__btn = document.getElementById("countIntegers__btn")
-let countIntegers__form = document.getElementById("countIntegers__form")
-let countIntegers__alert = document.getElementById("countIntegers__alert")
-// SO SÁNH
-let btn__compare = document.getElementById("btn__compare")
-let compare__btn = document.getElementById("compare__btn")
-let compare__form = document.getElementById("compare__form")
-let compare__alert = document.getElementById("compare__alert")
+function sum() {
+    openForm(1);
+    document.getElementById("sum__btn").onclick = function () {
+        let sum = 0;
+        let sum__alert = document.getElementById("sum__alert");
+        for (let i = 0; i < numArray.length; i++) {
+            if (numArray[i] >= 0) {
+                sum += numArray[i];
+            }
+        }
+        sum__alert.innerHTML = `Tổng số dương: ${sum}`;
+    }
+}
+// COUNT
+function count() {
+    openForm(2)
+    document.getElementById("count__btn").onclick = function () {
+        let count = 0;
+        let count__alert = document.getElementById("count__alert");
+        for (let i = 0; i < numArray.length; i++) {
+            if (numArray[i] > 0) {
+                count++;
+            }
+        }
+        count__alert.innerHTML = `số dương: ${count}`;
+    }
+}
+// tìm số nhỏ nhất
+function findMin() {
+    openForm(3)
+    document.getElementById("findMin__btn").onclick = function () {
+        let findMin__alert = document.getElementById("findMin__alert");
+        let min = numArray[0];
+        for (let i = 1; i < numArray.length; i++) {
+            if (min > numArray[i]) {
+                min = numArray[i];
+            }
+        }
+        findMin__alert.innerHTML = `số nhỏ nhất là: ${min}`;
+    }
+}
+// tìm số dương nhỏ nhất
+function findMinPositive() {
+    openForm(4);
+    document.getElementById("findMinPositive__btn").onclick = function () {
+        let findMinPositive__alert = document.getElementById("findMinPositive__alert");
+        let minPositive = numArray[0];
+        for (let i = 0; i < numArray.length; i++) {
+            if (numArray[i] > 0) {
+                if (minPositive > numArray[i]) {
+                    minPositive = numArray[i];
+                }
+            }
+        }
+        findMinPositive__alert.innerHTML = `số dương nhỏ nhất là: ${minPositive}`;
+    }
+}
+// số chẵn cuối cùng
+function numEven() {
+    openForm(5);
+    document.getElementById("numEven__btn").onclick = function () {
+        let varArchive = 0;
+        let numEven__alert = document.getElementById("numEven__alert");
+        for (i = (numArray.length - 1); i >= 0; i--) {
+            if (numArray[i] % 2 === 0) {
+                varArchive = numArray[i];
+                break;
+            }else{
+                varArchive = -1;
+            }
+        }
+        numEven__alert.innerHTML = `số chẵn cuối cùng là: ${varArchive}`
+    }
+}
+
+// Đổi chỗ
+
+function translocate() {
+    openForm(6);
+    document.getElementById("translocate__btn").onclick = function () {
+        let translocate__num1 = +document.getElementById("translocate__num1").value
+        let translocate__num2 = +document.getElementById("translocate__num2").value
+        let translocate__alert = document.getElementById("translocate__alert");
+        let temp = 0;
+        for (let i = 0; i < numArray.length; i++) {
+            temp = numArray[translocate__num1];
+            numArray[translocate__num1] = numArray[translocate__num2];
+            numArray[translocate__num2] = temp;
+        }
+
+        translocate__alert.innerHTML = `mảng sau khi đổi: `
+        for (let i = 0; i < numArray.length; i++) {
+            translocate__alert.innerHTML += `${numArray[i]}, `;
+        }
+    }
+}
+
+// Sắp xếp
+function arrange() {
+    openForm(7);
+    document.getElementById("arrange__btn").onclick = function () {
+        let arrange__alert = document.getElementById("arrange__alert");
+        let temp = 0;
+        for (let i = 0; i < (numArray.length - 1); i++) {
+            for (let j = i + 1; j < numArray.length; j++) {
+                if (numArray[i] > numArray[j]) {
+                    temp = numArray[i];
+                    numArray[i] = numArray[j];
+                    numArray[j] = temp;
+                }
+            }
+        }
+        arrange__alert.innerHTML = `mảng sau khi sắp xếp: `
+        for (let i = 0; i < numArray.length; i++) {
+            arrange__alert.innerHTML += `${numArray[i]}, `;
+        }
+    }
+}
+// tìm số nguyên tố đầu tiên
+// hàm kiểm tra số nguyên tố
+function checkPrime(x) {
+    if (x <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+function Prime() {
+    openForm(8);
+    document.getElementById("Prime__btn").onclick = function () {
+        let Prime__alert = document.getElementById("Prime__alert");
+        for (let i = 0; i < numArray.length; i++) {
+            if (checkPrime(numArray[i]) === true) {
+                Prime__alert.innerHTML = `${numArray[i]}`;
+                break;
+            }else{
+                Prime__alert.innerHTML = `-1`;
+            }
+        }
+    }
+}
+// đếm số nguyên
+function countIntegers() {
+    openForm(9);
+    let arrayCountIntegers = [];
+    let countIntegers__alert = document.getElementById("countIntegers__alert");
+    document.getElementById("addNum__btn").onclick = function () {
+        let addNum = +document.getElementById("addNum").value;
+        arrayCountIntegers.push(addNum);
+        let addNum__alert = document.getElementById("addNum__alert");
+        addNum__alert.innerHTML = `${arrayCountIntegers}`;
+    }
+    // console.log(arrayCountIntegers);
+    document.getElementById("countIntegers__btn").onclick = function () {
+        let countIntegers = 0;
+        for (let i = 0; i < arrayCountIntegers.length; i++) {
+            if ((Number.isInteger(arrayCountIntegers[i])) === true) {
+                countIntegers++;
+            }
+        }
+        console.log(countIntegers);
+        countIntegers__alert.innerHTML = `Số nguyên: ${countIntegers}`;
+    }
+}
+
+// so sánh số âm số dương
+function compare() {
+    openForm(10);
+    document.getElementById("compare__btn").onclick = function () {
+        let countPositive = 0;
+        let countNegative = 0;
+        let compare__alert = document.getElementById("compare__alert");
+        for (let i = 0; i < numArray.length; i++) {
+            if (numArray[i] > 0) {
+                countPositive++;
+            }else if(numArray[i] < 0){
+                countNegative++;
+            }
+        }
+        if(countNegative > countPositive){
+            compare__alert.innerHTML = `số âm > số dương`;
+        }else if(countPositive > countNegative){
+            compare__alert.innerHTML = `số dương > số âm`;
+        }else{
+            compare__alert.innerHTML = `số dương = số âm`;
+        }
+    }
+}
